5084e2ecf31609c141f9d5c95c3e4514db49adf7
Noman Faisal <nmnfaisal@gmail.com>
2023-08-29 13:05:09 +0500

    login ui done

--- saib_ui/lib/login.dart
+++ saib_ui/lib/login.dart
@@ -8,55 +8,72 @@ class MyLogin extends StatefulWidget {
 }
 
 class _MyLoginState extends State<MyLogin> {
-
-  bool _rememberMe = false;
   bool value = false;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
-        image: DecorationImage(image: AssetImage('assets/bg_landing.png'), fit: BoxFit.cover)),
+          image: DecorationImage(
+              image: AssetImage('assets/bg_landing.png'), fit: BoxFit.cover)),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         body: Stack(
-          // alignment: Alignment.center,
           children: [
-            Container(),
             Positioned(
-              // top: MediaQuery.of(context).size.height / 2 - 100, // Center vertically
-              // left: MediaQuery.of(context).size.width / 2 - 100, // Center horizontally
+              top: MediaQuery.of(context).size.height / 4 -
+                  100, // Center vertically
+              left: 0,
+              right: 0,
 
               child: Container(
-              // padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.25),
-              height: 130.0,
-              width: double.infinity, // Code to assign full width
-              decoration: const BoxDecoration(
-                image: DecorationImage( image: AssetImage('assets/line.png'), fit: BoxFit.fill),
-                shape: BoxShape.rectangle,
-                 ),
-              ),
-            ),
-
-
-            Positioned(
-              top: MediaQuery.of(context).size.height / 2 - 100, // Center vertically
-              left: MediaQuery.of(context).size.width / 2 - 100, // Center horizontally
-
-              child: Container(
-                // alignment: Alignment.center,
-                // padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5),
-                height: 50,
-                width: 250, // Code to assign full width
+                height: 130.0,
+                width: double.infinity, // Code to assign full width
                 decoration: const BoxDecoration(
-                  image: DecorationImage( image: AssetImage('assets/logo_saib.png'), fit: BoxFit.fill,),
+                  image: DecorationImage(
+                      image: AssetImage('assets/line.png'), fit: BoxFit.fill),
                   shape: BoxShape.rectangle,
                 ),
               ),
             ),
+            Positioned(
+              top: MediaQuery.of(context).size.height / 4 - 100, // Center vertically
+              left: 100,
+              child: Container(
+                height: 70,
+                width: 70,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/icons/ic_plane.png'),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+              ),
+            ),
+            Positioned(
+              top: MediaQuery.of(context).size.height / 2.5 -
+                  100, // Center vertically
+              left: 80,
+              right: 80,
 
+              child: Container(
+                height: 70,
+                width: 250, // Code to assign full width
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/logo_saib.png'),
+                    fit: BoxFit.fill,
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+              ),
+            ),
             Container(
-              padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5, right:35, left: 35),
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.4,
+                  right: 35,
+                  left: 35),
               child: Column(
                 children: [
                   const TextField(
@@ -73,135 +90,143 @@ class _MyLoginState extends State<MyLogin> {
                       contentPadding: EdgeInsets.all(20),
                     ),
                   ),
-
                   Row(
                     children: [
-                      buildRememberassword(),
+                      Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        // Adjust the padding values as needed
+                        child: buildRememberassword(),
+                      ),
                     ],
                   ),
-
-                  ElevatedButton(
-                    onPressed: () {
-                      // Perform login logic here
-                    },
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.yellow,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20.0),
+                  Padding(
+                    padding: EdgeInsets.all(20.0),
+                    // Adjust the padding values as needed
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Perform login logic here
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.yellow,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20.0),
+                        ),
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 40, vertical: 15),
                       ),
-                      padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                      child: const Text(
+                        'Login',
+                        style: TextStyle(fontSize: 18, color: Colors.black),
+                      ),
                     ),
-                    child: const Text(
-                      'Login',
-                      style: TextStyle(fontSize: 18, color: Colors.black),
-                    ),
                   ),
-
-                  const Text("Forgot Password", style: TextStyle(
-                      fontSize: 17.0,
-                      decoration: TextDecoration.underline,
-                      fontWeight: FontWeight.bold, color: Colors.white),
+                  const Text(
+                    "Forgot Password",
+                    style: TextStyle(
+                        fontSize: 17.0,
+                        decoration: TextDecoration.underline,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
                   ),
-
-
-                  const Text("Register Account", style: TextStyle(
-                      fontSize: 17.0,
-                      decoration: TextDecoration.underline,
-                      fontWeight: FontWeight.bold, color: Colors.white),
+                  const SizedBox(
+                    height: 20,
                   ),
-
-
-
-
+                  const Text(
+                    "Register Account",
+                    style: TextStyle(
+                        fontSize: 17.0,
+                        decoration: TextDecoration.underline,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
                 ],
               ),
             ),
-
-
-
             Positioned(
-              // top: MediaQuery.of(context).size.height / 2 - 100, // Center vertically
-              // left: MediaQuery.of(context).size.width / 2 - 100, // Center horizontally
-              bottom: 50,
-              left: 50,
-             child: Container(
-                // alignment: Alignment.center,
-                // padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5),
-                height: 50,
-                width: 50,
-                decoration: const BoxDecoration(
-                  image: DecorationImage( image: AssetImage('assets/icons/ic_aboutus.png'), fit: BoxFit.fill,),
-                  shape: BoxShape.rectangle,
-                ),
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(35.0),
+                    // Adjust the padding values as needed
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/icons/ic_aboutus.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: BoxShape.rectangle,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(35.0),
+                    // Adjust the padding values as needed
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/icons/ic_locator.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: BoxShape.rectangle,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(35.0),
+                    // Adjust the padding values as needed
+                    child: Container(
+                      height: 50,
+                      width: 50,
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage('assets/icons/ic_phone.png'),
+                          fit: BoxFit.fill,
+                        ),
+                        shape: BoxShape.rectangle,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-
-             ),
-
-            Positioned(
-              // top: MediaQuery.of(context).size.height / 2 - 100, // Center vertically
-              left: MediaQuery.of(context).size.width / 2 - 50 , // Center horizontally
-              bottom: 50,
-              // center: 50,
-              child: Container(
-                // alignment: Alignment.center,
-                // padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5),
-                height: 50,
-                width: 50,
-                decoration: const BoxDecoration(
-                  image: DecorationImage( image: AssetImage('assets/icons/ic_locator.png'), fit: BoxFit.fill,),
-                  shape: BoxShape.rectangle,
-                ),
-              ),
-
             ),
-
-            Positioned(
-              // top: MediaQuery.of(context).size.height / 2 - 100, // Center vertically
-              // left: MediaQuery.of(context).size.width / 2 - 100, // Center horizontally
-              bottom: 50,
-              right: 50,
-              child: Container(
-                // alignment: Alignment.center,
-                // padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.5),
-                height: 50,
-                width: 50,
-                decoration: const BoxDecoration(
-                  image: DecorationImage( image: AssetImage('assets/icons/ic_phone.png'), fit: BoxFit.fill,),
-                  shape: BoxShape.rectangle,
-                ),
-              ),
-
-            )
-
-
           ],
         ),
       ),
     );
   }
 
-  Widget buildRememberassword(){
+  Widget buildRememberassword() {
     return Container(
       height: 20,
       child: Row(
         children: [
-          Theme(data: ThemeData(unselectedWidgetColor: Colors.white),
+          Theme(
+              data: ThemeData(unselectedWidgetColor: Colors.white),
               child: Checkbox(
-                value:  false,
+                value: false,
                 checkColor: Colors.blueGrey,
                 activeColor: Colors.white,
-                onChanged: (value)
-                {
+                onChanged: (value) {
                   setState(() {
                     // rememberpwd=value;
                   });
                 },
               )),
-          const Text("Remember me", style: TextStyle(
-              fontWeight: FontWeight.bold, color: Colors.white),),
+          const Text(
+            "Remember me",
+            style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+          ),
         ],
       ),
     );
   }
-
 }
